24c9493f1ba9a53ce19eb6e3002b47c1751c8aeb
priyan <priyan.abewickrama@ideahub.lk>
2023-04-23 18:16:23 +0530

    cv

--- src/components/CvBuilder/UiCv.jsx
+++ src/components/CvBuilder/UiCv.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { UiRow } from "../Common/UiRow";
+import { UiCol } from "../Common/UiCol";
+import profileImg from '../../styles/img/profile.jpg';
 
-export const UiCv = () => {
+export const UiCv = (props) => {
     return (
         <>
-            <h2>CV</h2>
+            <section className="pd-cv-format pd-padd">
+                <div className="pd-cv-top-bar">
+                    <div className="pd-cv-top-bar__left">
+                        <h2>Lorem Ipsum</h2>
+                        <p>Web Developer</p>
+                    </div>
+                    <div className="pd-cv-top-bar__right">
+                        <div>
+                            <p><strong>Email: </strong>priyan@gmail.com</p>
+                            <p><strong>Address: </strong>No.120, Kottawa, Pannipitiya.</p>
+                            <p><strong>Phone: </strong>0741251125</p>
+                        </div>
+                        <img src={profileImg} />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
