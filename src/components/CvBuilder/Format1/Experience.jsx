9e76ae71e7feeb87257b44632678bec1a0427073
priyan <priyan.abewickrama@ideahub.lk>
2023-04-24 19:49:52 +0530

    experience items added

--- src/components/CvBuilder/Format1/Experience.jsx
+++ src/components/CvBuilder/Format1/Experience.jsx
@@ -5,24 +5,15 @@ export const Experience = (props) => {
     return (
         <>
             <div className="pd-cv-wrapper__inner-desc pd-flx-space-between align-items-top">
-                {/* <div className="pd-cv-left">
-                    <h4 className="cv-sec-title">Experience</h4>
-                    <p className="pd-ls-loc">
-                        San Fransisco, CA
-                    </p>
-                    <p className="pd-ls-duration">
-                        Jan 2013 - Mar 2017
-                    </p>
-                </div>
-                <div className="pd-cv-right">
-                    <div className="pd-desc-lg">
-                     <h5>Sales Associate</h5>  
-                     <h6>Louis Vuitton</h6>
-                     <p>experience in crafting and testing user-friendly interfaces, debugging and updating existing projects</p>
-
-                    </div>
-                </div> */}
-                <UiCvItem> </UiCvItem>
+              
+                <UiCvItem
+                    title="Experience"
+                    location="San Fransisco, CA"
+                    duration="Jan 2015 - Mar 2018">
+                    <h5>Sales Associate</h5>
+                    <h6>Louis Vuitton</h6>
+                    <p>experience in crafting and testing user-friendly interfaces, debugging and updating existing projects</p>
+                </UiCvItem>
             </div>
         </>
     )
