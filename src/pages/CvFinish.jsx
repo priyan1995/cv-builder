256e14a945c73bdb536cc601843e7645418d67a3
priyan <priyan.abewickrama@ideahub.lk>
2023-05-16 14:57:44 +0530

    cv download page added

--- src/pages/CvFinish.jsx
+++ src/pages/CvFinish.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { WebHeader } from "../components/Common/WebHeader";
 import { UiContainer } from "../components/Common/UiContainr";
 import { UiRow } from "../components/Common/UiRow";
@@ -21,6 +21,7 @@ import { SkillsForm } from "../components/CvBuilder/Forms/SkillsForm";
 import { OtherData } from "../mockData/Other";
 import { OtherForm } from "../components/CvBuilder/Forms/OtherForm";
 import { UiButton } from "../components/Common/UiButtons";
+import { useReactToPrint } from "react-to-print";
 
 export const CvFinish = () => {
 
@@ -40,184 +41,70 @@ export const CvFinish = () => {
     const [email, setEmail] = useState('priyan@gmail.com');
     const [address, setAddress] = useState('No.120, Kottawa, Pannipitiya.');
     const [phone, setPhone] = useState('0741251125');
-    const [date, setDate] = useState();
 
     // objectives
     const [objective, setObjective] = useState(objs);
 
     // experience
     const [experience, setExperience] = useState(experienceArr);
-    const [position, setPosition] = useState('');
-    const [compName, setCompanyName] = useState('');
-    const [location, setLocation] = useState('');
-    const [period, setPeriod] = useState('');
-    const [jobDescription, setJobDescription] = useState('');
 
     // education
     const [education, setEducation] = useState(educationeArr);
-    const [degree, setDegree] = useState('');
-    const [university, setUniversity] = useState('');
-    const [locationEdu, setLocationEdu] = useState('');
-    const [periodEdu, setPeriodEdu] = useState('');
-    const [eduDesc, setEduDesc] = useState('');
 
     // langauges
     const [lang, setLang] = useState(languagesArr);
-    const [language, setLanguage] = useState('');
-    const [skillLevel, setSkillLevel] = useState('');
-    const [langDesc, setLangDesc] = useState('');
 
     // skills
     const [skills, setSkills] = useState(skillArr);
-    const [skillTitle, setSkillTitle] = useState('');
 
     // other
     const [otherTitle, setOtherTitle] = useState(otherFTitle);
     const [otherDesc, setOtherDesc] = useState(otherFDesc);
 
 
-    // wizard steps
-    const [viewPersonalInfo, setViewPersonalInfo] = useState(true);
-    const [viewObjectives, setViewObjectives] = useState(false);
-    const [viewExperience, setViewExperience] = useState(false);
-    const [viewEducation, setViewEducation] = useState(false);
-    const [viewLang, setViewLang] = useState(false);
-    const [viewSkills, setViewSkills] = useState(false);
-    const [viewOther, setViewOther] = useState(false);
+    const componentRef = useRef();
 
-    // personal Infomation Form
-    const OnsaveFormOne = () => {
-        console.log('saved Contact');
-        setViewPersonalInfo(false);
-        setViewObjectives(true);
-    }
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: 'Cv',
+        onAfterPrint: () => alert("Print Success")
+    })
 
-    // objectives Form
-    const OnsaveObjective = () => {
-        console.log('saved Objective');
-        setViewObjectives(false);
-        setViewExperience(true);
-    }
-
-    // experience form next
-    const nextFromExp = () => {
-        setViewExperience(false);
-        setViewEducation(true);
-    }
-
-    const nextFromEdu = () => {
-        setViewEducation(false);
-        setViewLang(true);
-    }
-
-    // Langauge form next
-    const nextFromLang = () => {
-        setViewLang(false);
-        setViewSkills(true);
-    }
-
-    // skills form next
-    const nextFromSkills = () => {
-        setViewSkills(false);
-        setViewOther(true);
-    }
-
-
-    // save Experience data
-    const onSaveExp = (e) => {
-        setPosition(e.target.value);
-        setCompanyName(e.target.value);
-        setLocation(e.target.value);
-        setPeriod(e.target.value);
-        setJobDescription(e.target.value);
-
-        const newElement = { id: '3', position: position, companyName: compName, location: location, period: period, desc: jobDescription }
-
-        setExperience(experience => [...experience, newElement]);
-    }
-
-    // save education data
-    const onSaveEduc = (e) => {
-        setDegree(e.target.value);
-        setUniversity(e.target.value);
-        setLocationEdu(e.target.value)
-        setPeriodEdu(e.target.value);
-        setEduDesc(e.target.value);
-
-        const newEduElement = { id: '3', degree: degree, uni: university, location: locationEdu, period: periodEdu, desc: eduDesc }
-
-        setEducation(education => [...education, newEduElement]);
-
-        console.log(newEduElement);
-    }
-
-    // save language data
-    const onSaveLang = (e) => {
-
-        setLanguage(e.target.value);
-        setSkillLevel(e.target.value);
-        setLangDesc(e.target.value);
-
-        const newLang = { id: '3', language: language, skillLevel: skillLevel, desc: langDesc };
-        setLang(lang => [...lang, newLang]);
-    }
-
-    // save skill
-    const onSaveSkill = (e) => {
-        setSkillTitle(e.target.value);
-
-        const newSkill = { id: '13', name: skillTitle, };
-        setSkills(skills => [...skills, newSkill]);
-    }
-
-    // save Other
-    const onSaveOther = (e) => {
-        setOtherTitle(otherTitle);
-        setOtherDesc(otherDesc);
-    }
-
-    // remove experience item
-    const removeExperienceItem = (id) => {
-        setExperience(experience => experience.filter(experience => experience.id !== id));
-    }
-    // remove education item
-    const removeEducationItem = (id) => {
-        setEducation(education => education.filter(education => education.id !== id));
-    }
-    // remove language item
-    const removeLanguage = (id) => {
-        setLang(lang => lang.filter(lang => lang.id !== id));
-    }
-    // remove Skill
-    const removeSkill = (id) => {
-        setSkills(skills => skills.filter(skills => skills.id !== id));
-    }
 
 
     return (
         <>
             <WebHeader />
-            <UiContainer fluid={true} >
-                <UiRow className="pd-m-0 pd-cv-finish">
-                    <UiCol lg="12" className="pd-padd  pd-cv-right-6">
-                        <UiCv
-                            firstName={firstName}
-                            lastName={lastName}
-                            occupation={occupation}
-                            email={email}
-                            address={address}
-                            phone={phone}
-                            objective={objective}
-                            exper={experience}
-                            education={education}
-                            lang={lang}
-                            skillData={skills}
-                            otherFTitle={otherTitle}
-                            otherFDesc={otherDesc}
-                        />
-                    </UiCol>
-                    <UiCol lg="12 pd-download">
-                        <UiButton className="pd-primary-btn pd-large-pad " > Download</UiButton>
+            <div ref={componentRef} >
+                <UiContainer fluid={true}>
+                    <UiRow className="pd-m-0 pd-cv-finish">
+                        <UiCol lg="12" className="pd-padd  pd-cv-right-6">
+                            <UiCv
+                                firstName={firstName}
+                                lastName={lastName}
+                                occupation={occupation}
+                                email={email}
+                                address={address}
+                                phone={phone}
+                                objective={objective}
+                                exper={experience}
+                                education={education}
+                                lang={lang}
+                                skillData={skills}
+                                otherFTitle={otherTitle}
+                                otherFDesc={otherDesc}
+                            />
+                        </UiCol>
+
+                    </UiRow>
+                </UiContainer>
+
+
+            </div>
+            <UiContainer>
+                <UiRow>
+                    <UiCol lg="12 pd-download text-center">
+                        <UiButton className="pd-primary-btn pd-large-pad " onClick={handlePrint}> Download</UiButton>
                     </UiCol>
                 </UiRow>
             </UiContainer>
