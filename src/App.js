106db53568839f826a7bcd45251d9ffa35627394
priyan1995 <priyan.abewickrama@ideahub.lk>
2025-06-22 12:00:07 +0530

    hasrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/styles/style.scss';
